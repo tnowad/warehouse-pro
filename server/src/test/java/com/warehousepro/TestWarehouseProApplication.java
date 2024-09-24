54cf5d0090eaeb552e18b3ccfd0f17647a5f364c
Nguyen Minh Tuan <tnowad@gmail.com>
2024-09-24 13:55:59 +0700

    feat: initial project

---
+++ server/src/test/java/com/warehousepro/TestWarehouseProApplication.java
@@ -0,0 +1,11 @@
+package com.warehousepro;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestWarehouseProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(WarehouseProApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
---
+++ server/src/test/java/com/warehousepro/WarehouseProApplicationTests.java
@@ -0,0 +1,15 @@
+package com.warehousepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class WarehouseProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
