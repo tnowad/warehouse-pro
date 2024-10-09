37005857fb888eebb17f3f258967752ff46ae65d
Nguyen Minh Tuan <tnowad@gmail.com>
2024-10-09 21:21:38 +0700

    feat: add get order status

--- client/src/app/_components/order-status-card.tsx
+++ client/src/app/_components/order-status-card.tsx
@@ -1,3 +1,27 @@
+import { DataTable } from "@/components/ui/data-table";
+import { getOrderStatus } from "@/server/actions/order";
+import { ColumnDef } from "@tanstack/react-table";
+import { useQuery } from "react-query";
+
 export function OrderStatusCard() {
-  return <div>OrderStatusCard</div>;
+  const getOrderStatusQuery = useQuery("order-status", () => getOrderStatus());
+
+  const columns: ColumnDef<{
+    id: string;
+    name: string;
+    status: string;
+  }>[] = [
+    { accessorKey: "id", header: "ID" },
+    { accessorKey: "name", header: "Name" },
+    { accessorKey: "status", header: "Status" },
+  ];
+
+  return (
+    <div>
+      <DataTable
+        columns={columns}
+        data={getOrderStatusQuery.data?.items ?? []}
+      />
+    </div>
+  );
 }
