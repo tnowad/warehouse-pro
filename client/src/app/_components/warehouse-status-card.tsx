e253e0577e452606e2f2a5c849a20a99d51be747
Nguyen Minh Tuan <tnowad@gmail.com>
2024-10-09 20:55:17 +0700

    feat: add warehouse status chart

--- client/src/app/_components/warehouse-status-card.tsx
+++ client/src/app/_components/warehouse-status-card.tsx
@@ -1,21 +1,86 @@
+"use client";
 import { useQuery } from "react-query";
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTable } from "@/components/ui/data-table";
 import { Button } from "@/components/ui/button";
 import { Eye } from "lucide-react";
-import { getWarehouseStatus } from "@/server/actions/warehouse";
+import {
+  getWarehouseStatus,
+  getWarehouseStatusCount,
+} from "@/server/actions/warehouse";
 import { useRouter } from "next/navigation";
+import { Cell, Pie, PieChart } from "recharts";
 
-export function WarehouseStatusCard() {
-  const { data: warehouses, isLoading } = useQuery(["warehouses"], () => {
-    return getWarehouseStatus();
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartConfig = {
+  count: {
+    label: "Count",
+  },
+  active: {
+    label: "Active",
+    color: "hsl(var(--chart-1))",
+  },
+  inactive: {
+    label: "Inactive",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+
+export function WarehouseStatusChart() {
+  const getWarehouseStatusCountQuery = useQuery(["warehouses-count"], () => {
+    return getWarehouseStatusCount();
   });
 
-  const router = useRouter();
+  return (
+    <Card className="flex flex-col mb-2">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Warehouse Status Pie Chart</CardTitle>
+        <CardDescription>Current Status Overview</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={getWarehouseStatusCountQuery.data ?? []}
+              dataKey="count"
+              nameKey="status"
+              innerRadius={60}
+            >
+              <Cell fill={chartConfig.active.color} key="active" />
+              <Cell fill={chartConfig.inactive.color} key="inactive" />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+export function WarehouseStatusCard() {
+  const getWarehouseStatusQuery = useQuery(["warehouses"], () => {
+    return getWarehouseStatus();
+  });
+  const router = useRouter();
 
   const columns: ColumnDef<{ id: number; name: string; status: string }>[] = [
     {
@@ -51,7 +116,11 @@ export function WarehouseStatusCard() {
 
   return (
     <div>
-      <DataTable columns={columns} data={warehouses ?? []} />
+      <WarehouseStatusChart />
+      <DataTable
+        columns={columns}
+        data={getWarehouseStatusQuery.data?.items ?? []}
+      />
     </div>
   );
 }
