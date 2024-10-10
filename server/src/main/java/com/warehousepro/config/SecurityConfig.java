535192776bfe76b14554d576332b937809b6ad83
quachhuuvinh <vhuu3208@gmail.com>
2024-10-10 21:23:05 +0700

    fix security config fix pom.xml

--- server/src/main/java/com/warehousepro/config/SecurityConfig.java
+++ server/src/main/java/com/warehousepro/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.warehousepro.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,13 +14,16 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 @EnableMethodSecurity
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private CustomJwtDecoder customJwtDecoder;
 
     @Bean
     public DefaultSecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer ->
+                        jwtConfigurer.decoder(customJwtDecoder)) )
                 .build();
     }
 
