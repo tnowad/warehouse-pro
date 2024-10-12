dbd79d6bf22503f1f92ff48503ef4419fa9b0673
quachhuuvinh <vhuu3208@gmail.com>
2024-10-12 21:25:49 +0700

    feature openApi test

--- server/src/main/java/com/warehousepro/controller/UserController.java
+++ server/src/main/java/com/warehousepro/controller/UserController.java
@@ -2,8 +2,14 @@ package com.warehousepro.controller;
 
 import com.warehousepro.dto.request.auth.CreateUserRequest;
 import com.warehousepro.dto.response.auth.UserResponse;
+import com.warehousepro.entity.User;
 import com.warehousepro.mapstruct.UserMapper;
 import com.warehousepro.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -27,6 +33,18 @@ public class UserController {
         .build();
   }
 
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found the book",
+            content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = User.class)) }),
+        @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+            content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = Error.class)) }),
+
+        @ApiResponse(responseCode = "404", description = "Book not found",
+            content = {@Content(mediaType = "application/json",
+                schema = @Schema(implementation = Error.class))})})
   @GetMapping("/{userId}")
   UserResponse getUser(@PathVariable String userId) {
     var user = userService.getUser(userId);
