012fd6d6a162c1cde9721f4b36c1ee0a7cbb940e
Nguyen Minh Tuan <tnowad@gmail.com>
2024-10-16 20:54:35 +0700

    feat: add cors

--- server/src/main/java/com/warehousepro/config/SecurityConfig.java
+++ server/src/main/java/com/warehousepro/config/SecurityConfig.java
@@ -1,14 +1,18 @@
+
 package com.warehousepro.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableMethodSecurity
 @EnableWebSecurity
@@ -16,13 +20,31 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
-  public DefaultSecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-    return httpSecurity.csrf(AbstractHttpConfigurer::disable).build();
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .csrf(csrf -> csrf.disable()).authorizeHttpRequests(
+            auth -> auth.requestMatchers("/**").permitAll().anyRequest().authenticated());
+    return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(List.of("Authorization", "Requestor-Type", "content-type",
+        "Access-Control-Request-Method", "Origin", "X-Requested-With", "X-HTTP-Method-Override",
+        "Accept", "Accept-Encoding", "No-Auth"));
+    configuration.setExposedHeaders(List.of("X-Get-Header"));
+    configuration.setMaxAge(3600L);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-
 }
