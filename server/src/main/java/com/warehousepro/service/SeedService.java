e023247f7116951a20ee9873b8b7d274b75a3a45
Nguyen Minh Tuan <tnowad@gmail.com>
2024-10-21 15:27:08 +0700

    feat: add seed warehouse

--- server/src/main/java/com/warehousepro/service/SeedService.java
+++ server/src/main/java/com/warehousepro/service/SeedService.java
@@ -1,21 +1,31 @@
 package com.warehousepro.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.warehousepro.entity.User;
+import com.warehousepro.entity.Warehouse;
 import com.warehousepro.repository.UserRepository;
+import com.warehousepro.repository.WareHouseRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class SeedService {
 
   final UserRepository userRepository;
+  final WareHouseRepository wareHouseRepository;
   final BCryptPasswordEncoder passwordEncoder;
+  final Faker faker = new Faker();
 
   @Transactional
   public void clearSeeds() {
+    wareHouseRepository.deleteAllInBatch();
     userRepository.deleteAllInBatch();
   }
 
@@ -24,8 +34,8 @@ public class SeedService {
     // TODO: Seed permissions
     // Seed: roles
     // Seed: users
-    User[] users = {User.builder().email("admin@warehouse-pro.com").name("Admin")
-        .password(passwordEncoder.encode("Password@123")).build()};
+    List<User> users = List.of(User.builder().email("admin@warehouse-pro.com").name("Admin")
+        .password(passwordEncoder.encode("Password@123")).build());
     for (User user : users) {
       userRepository.save(user);
     }
@@ -33,6 +43,12 @@ public class SeedService {
 
   @Transactional
   public void mockSeeds() {
-
+    List<Warehouse> warehouses = new ArrayList<>();
+    for (int i = 0; i < 63; i++) {
+      warehouses.add(
+          Warehouse.builder().name(faker.company().name()).location(faker.address().fullAddress())
+              .capacity(faker.number().numberBetween(0, 3000)).build());
+    }
+    wareHouseRepository.saveAll(warehouses);
   }
 }
